a575a40a4e3afaf28a321ae26243c7c3b981ff87
Lydtk <47219609+Lydtk@users.noreply.github.com>
2019-09-20 18:21:57 +0100

    Function headers only
    Function headers only put in.

--- Lowest Common Ancestor1/src/LCA.java
+++ Lowest Common Ancestor1/src/LCA.java
@@ -1,4 +1,33 @@
 
 public class LCA {
-  
-}
+	class Node { 
+	     
+	  
+	    
+	} 
+	  
+	public static class BT_NoParentPtr_Solution1  
+	{ 
+	  
+	    
+	} 
+	  
+	private int findCurrLCA() { 
+	  
+	   return 1;
+    } 
+	      
+	    
+	private boolean findPath() 
+	{ 
+		return true;
+	}
+	  
+	    
+    public static void main(String[] args) 
+    { 
+       
+      
+    } 
+	} 
+
