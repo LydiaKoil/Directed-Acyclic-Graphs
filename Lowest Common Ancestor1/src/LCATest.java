88d4067884c0635be6f2693ba0d4a26523815b01
Lydtk <47219609+Lydtk@users.noreply.github.com>
2019-09-23 17:28:59 +0100

    testConstructorTree added
    Code unalteres

--- Lowest Common Ancestor1/src/LCATest.java
+++ Lowest Common Ancestor1/src/LCATest.java
@@ -1,12 +1,33 @@
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.Test;
 
 class LCATest {
-
 	
 	@Test
-	void testBasicFunc() {
+	void testConstructorTree() {
+		LCA tree= new LCA();
+	    tree.root = new Node(1); 
+	    tree.root.left = new Node(2); 
+	    tree.root.right = new Node(3); 
+	    tree.root.left.left = new Node(4); 
+	    tree.root.left.right = new Node(5); 
+	    tree.root.right.left = new Node(6); 
+	    tree.root.right.right = new Node(7); 
+	    
+	    
+	    assertEquals(1, tree.root.data);
+	    assertEquals(2, tree.root.left.data);
+	    assertEquals(3, tree.root.right.data); 
+	    assertEquals(4, tree.root.left.left.data); 
+		
+	}
+	
+	
+	
+	@Test
+	void testfindLCAInternal() {
 		LCA tree = new LCA(); 
 	    tree.root = new Node(1); 
 	    tree.root.left = new Node(2); 
@@ -21,6 +42,7 @@ class LCATest {
 	    assertEquals(1, tree.findLCA(3,4));
 	    assertEquals(2, tree.findLCA(2,4));
 	}
+	
 	
 	@Test
 	void testNonExNum() {
@@ -38,5 +60,7 @@ class LCATest {
 	    assertEquals(-1, tree.findLCA(20,10)); //both numbers missing
 	    
 	}
+	
+	
 	
 }
